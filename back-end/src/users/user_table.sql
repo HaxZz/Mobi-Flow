59b9e9e0043306043a76a2ac7d38792ca2b9ffd7
Tendry <rakotondramasy@ecole.ensicaen.fr>
2017-01-11 17:47:27 +0100

    Oups j'avais pas SRC

--- back-end/src/users/user_table.sql
+++ back-end/src/users/user_table.sql
@@ -1,9 +1,11 @@
+DROP TABLE user;
 CREATE TABLE user
 (
-	username VARCHAR(30),
-	password VARCHAR(30),
-	disabled CHAR,
-	firstname VARCHAR(30),
-	lastname VARCHAR(30),
-	email_addr VARCHAR(30)
+	id 			INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	username 	VARCHAR(30),
+	password 	VARCHAR(30),
+	disabled 	CHAR,
+	firstname 	VARCHAR(30),
+	lastname 	VARCHAR(30),
+	email_addr 	VARCHAR(30)
 );
