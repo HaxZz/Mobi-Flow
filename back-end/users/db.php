cae7167147098179e85892e35296332fc33b0804
Tendry <rakotondramasy@ecole.ensicaen.fr>
2017-01-11 16:31:58 +0100

    Adds DB table definition

--- back-end/users/db.php
+++ back-end/users/db.php
@@ -14,4 +14,34 @@
 		return new PDO('mysql:host=localhost;dbname=mobiflow', $user, $pass);
 	}
 
+	/**
+	 * @brief Signs a new user into the database 
+	 * @param  User $user the user inserted in the database
+	 * @return Success or fail       
+	 */
+	function signUp($user)
+	{
+		$pdo = connect();
+
+		$sql = "INSERT INTO mabite";
+
+		$pdo = null;
+	}
+
+	/**
+	 * @brief logs the given user in
+	 * @param  User $user the user to be logged in
+	 * @return Success or fail
+	 */
+	function login($user)
+	{
+		$pdo = connect();
+
+
+
+		$pdo = null;
+	}
+
+	 
+	//modify pass, modify mail
 ?>
