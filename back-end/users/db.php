0cae52aab85b08ee386848ee179a3f28af5a3eac
Tendry <rakotondramasy@ecole.ensicaen.fr>
2017-01-11 17:04:55 +0100

    Database Method prototypes done

--- back-end/users/db.php
+++ back-end/users/db.php
@@ -38,10 +38,34 @@
 		$pdo = connect();
 
 
-
 		$pdo = null;
 	}
 
-	 
-	//modify pass, modify mail
+	/**
+	 * @brief Modifies the password of the user
+	 * @param  User   $user         the user that changes his password
+	 * @param  String $newPassword  the new password
+	 * @return Success or fail
+	 */
+	function modifyPass($user, $newPassword)
+	{
+		$pdo = connect();
+
+
+		$pdo = null;
+	} 
+
+	/**
+	 * @brief Modifies the email adress of the user
+	 * @param  User $user      The user modified
+	 * @param  String $newMail The new email adress
+	 * @return Success or fail
+	 */
+	function modifyMail($user, $newMail)
+	{
+		$pdo = connect();
+
+
+		$pdo = null;
+	}
 ?>
