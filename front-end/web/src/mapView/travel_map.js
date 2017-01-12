6a7e8307e852de870bce8a285968df7e93eaa41b
Tendry <rakotondramasy@ecole.ensicaen.fr>
2017-01-12 15:14:39 +0100

    Map adjustments
    DIspaly marker & pictures for them

--- front-end/web/src/mapView/travel_map.js
+++ front-end/web/src/mapView/travel_map.js
@@ -8,6 +8,18 @@
 
 var MAP_HTML_ID = "map";
 
+var map = null;
+
+function getMap()
+{
+	if(map == null)
+	{
+		map = L.map(MAP_HTML_ID);
+	}
+
+	return map;
+}
+
 function draw_initial_map_error()
 {
     'use strict';
@@ -26,47 +38,20 @@ function draw_initial_map_error()
 
 function draw_initial_map_unsafe()
 {
-    var mymap = L.map(MAP_HTML_ID).setView([49.1846225, -0.4073643], 13);
+    var map = getMap().setView([49.1846225, -0.4073643], 13);
     
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
-		{
+	{
 		    attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 		    maxZoom: 18,
 		    id: mapBoxId, //mapBoxId defined elsewhere
 		    accessToken: mapBoxToken //mapBoxToken defined elsewhere
-		}).addTo(mymap);
-
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: mapBoxId, //mapBoxId defined elsewhere
-    accessToken: mapBoxToken //mapBoxToken defined elsewhere
+	}).addTo(map);
 }
 
-function placeMarker(adress)
+function placeMarker(latitude, longitude)
 {
-	'use strict';
-    
-	var words = adress.split(" ");
-
-    var request = getXMLHttpRequest();
-    var method = 'GET';
-    request.open(method, TRAVEL_BACKEND_URL, true);
-    request.onreadystatechange = function()
-    {
-	'use strict';
-	if(request.readyState == 4)
-	{
-	    if(request.status == 200)
-	    {
-		var json_received_string = request.reponseText.trim();
-		var json_received = JSON.parse(json_received_string);
-		// TODO
-	    }
-	}
-    };
-    var json_to_send = get_json_to_send_for_travel_from_form();
-    sendXMLHttpRequest(request, json_to_send);
-
+	var marker = L.marker([latitude, longitude]).addTo(getMap());
 }
 
 function draw_initial_map()
