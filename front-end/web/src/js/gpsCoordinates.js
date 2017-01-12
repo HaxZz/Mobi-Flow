98912ef1ec7b08d447766cb7cbac7bccd95e21ec
Nicola Spanti (RyDroid) <dev@nicola-spanti.info>
2017-01-12 16:14:12 +0100

    Improving gpsCoordinates.js

--- front-end/web/src/js/gpsCoordinates.js
+++ front-end/web/src/js/gpsCoordinates.js
@@ -1,48 +1,116 @@
+function getGPScoordinatesFromXml(xml)
+{
+    'use strict';
+    
+    var placeTags = xml.getElementsByTagName("place");
+    if(!placeTags || placeTags.length == 0)
+    {
+	return null;
+    }
+    
+    var placeTag = placeTags[0];
+    if(!placeTag)
+    {
+	return null;
+    }
+    
+    return {
+	"latitude" : placeTag.getAttribute("lat"),
+	"longitude": placeTag.getAttribute("lon")
+    };
+}
+
+function getGPScoordinatesFromRequestOk(request)
+{
+    'use strict';
+    
+    if(request && request.responseXML)
+    {
+	return getGPScoordinatesFromXml(request.responseXML);
+    }
+    
+    console.error(request);
+    return null;
+}
+
+function gps_coordinates_request_statechange(request)
+{
+    'use strict';
+    
+    if(!isXMLHttpRequest(request))
+    {
+	if(isXMLHttpRequest(this))
+	{
+	    request = this;
+	}
+	else if(typeof(this) == "object" &&
+		"target" in this &&
+	       isXMLHttpRequest(this.target))
+	{
+	    request = this.target;
+	}
+	else
+	{
+	    throw new Exception("no argument");
+	}
+    }
+    
+    if(request.readyState == 4)
+    {
+	if(request.status == 200)
+	{
+	    return getGPScoordinatesFromRequestOk(request);
+	}
+	else
+	{
+	    console.error(request);
+	}
+    }
+    return null;
+}
+
 /**
- * @brief  gets the GPS coordinates given an adress string
- * @param  String adress a string describing a street adress, give a ZIP code a city a street name
+ * @brief  gets the GPS coordinates given an address string
+ * @param  String address a string describing a street adress, give a ZIP code a city a street name
  * @return An array containing the values like : ["latitude"=value, "longitude"=value]
  */
-function getGPScoordinates(adress)
+function getGPScoordinates(address)
 {
-	'use strict';
+    'use strict';
     
-	var words = adress.split(" ");
-	var queryargs = "";
-	for (var i = 0; i < words.length; i++) 
-	{
-		queryargs += words[i];
-		if(i != words.length-1)
-			queryargs += "+";
-	}
-
-	queryargs += "&format=xml&polygon=1&addressdetails=1";
-	var ApiURL = "http://nominatim.openstreetmap.org/search?q=";
-	ApiURL += queryargs;
-
-    var request = getXMLHttpRequest();
-    var method = 'GET';
-    request.open(method, ApiURL, false);
-
-    var gps = 
+    var gps =
     {
-    	"latitude":null,
-    	"longitude":null
+    	"latitude" : null,
+    	"longitude": null
     };
+    
+    var words = address.split(" ");
+    var queryargs = "";
+    for (var i = 0; i < words.length; i++)
+    {
+	queryargs += words[i];
+	if(i != words.length - 1)
+	{
+	    queryargs += "+";
+	}
+    }
 
+    queryargs += "&format=xml&polygon=1&addressdetails=1";
+    var apiURL = "https://nominatim.openstreetmap.org/search?q=";
+    apiURL += queryargs;
+    
+    var method = 'GET';
+    var request = getXMLHttpRequest();
+    if(!request)
+    {
+	throw new Error("No XMLHttpRequest");
+    }
+    request.open(method, apiURL, false);
     request.onreadystatechange = function()
     {
-		'use strict';
-		if(request.readyState == 4)
-		{
-		    if(request.status == 200)
-		    {
-				var xml = request.responseXML;
-				gps["latitude"] = xml.getElementsByTagName("place")[0].getAttribute("lat"); 
-				gps["longitude"] = xml.getElementsByTagName("place")[0].getAttribute("lon");		    }
-		}
+	'use strict';
+	gps = gps_coordinates_request_statechange(request);
     };
-    var json_to_send = "";
-    sendXMLHttpRequest(request, json_to_send);
+    sendXMLHttpRequest(request);
     return gps;
 }
