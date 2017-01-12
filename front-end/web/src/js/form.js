9403652aa09337ad60b98c294a750534702a4ba0
Nicola Spanti (RyDroid) <dev@nicola-spanti.info>
2017-01-12 19:20:44 +0100

    The page for the date is managed by the web front-end

--- front-end/web/src/js/form.js
+++ front-end/web/src/js/form.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016  Nicola Spanti (also known as RyDroid) <dev@nicola-spanti.info>
+ * Copyright (C) 2016  Nicola Spanti <dev@nicola-spanti.info>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
