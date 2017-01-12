f304c5b4875bbc57cd1f2ad0dfdf370e2a5c31f1
Lebecachel Pierre <lebecachel@ecole.ensicaen.fr>
2017-01-12 21:23:25 +0100

    vroum vroum

--- back-end/src/mobiflow_api/booking.php
+++ back-end/src/mobiflow_api/booking.php
@@ -8,5 +8,5 @@ $bddData = array(
     'date' => date("d/m/Y-HH:ii")
 );
 //TODO requete BDD pour inserer la reservation
-//TODO convenir du retour avec Spantu
+//TODO convenir du retour avec Spantu ( Dire 'OK' ou les errors)
 //echo $result;
