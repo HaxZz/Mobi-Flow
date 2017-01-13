98a0fa97bd70536acbab346cef3ebc47bdb28828
Lebecachel Pierre <lebecachel@ecole.ensicaen.fr>
2017-01-13 01:34:04 +0100

    Enfin ça marche, plus besoin d'envoyer ma bite

--- back-end/src/mobiflow_api/booking.php
+++ back-end/src/mobiflow_api/booking.php
@@ -1,12 +1,12 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-$jsonInput = file_get_contents('php://input');
+$jsonInput = $_POST['json'];
 $dataReceived = json_decode($jsonInput, true);
-$userID = $dataReceived['user-id'];
+$userID = $_POST['user-id'];
 $bddData = array(
     'user-id' => $userID,
     'date' => date("d/m/Y-HH:ii")
 );
 //TODO requete BDD pour inserer la reservation
 //TODO convenir du retour avec Spantu ( Dire 'OK' ou les errors)
-echo json_encode(array("Penis" => "Gros"));
+echo "Ma Bite";
